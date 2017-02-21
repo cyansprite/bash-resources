fd32b371498c6623105034c347be940565b4d329
joj <brandoncoffman.cs@gmail.com>
2017-02-21 14:49:43 -0500

    Finished install script!

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 SOURCE=${PWD}
+
 #make sure they have pathogen...
 PATHOGEN=~/.config/nvim/autoload
 if [ ! -d "$PATHOGEN" ] ; then
-	echo "curling pathogen!"
+	echo "================================curling pathogen! "
 	mkdir -p ~/.vim/autoload ~/.vim/bundle
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 else
-	echo "Already have pathogen; if not delete autoload, and rerun this."
+	echo "================================Already have pathogen; if not delete autoload, and rerun this. "
 fi
 
 #make sure we have all the plugins we need!
 declare -a GITURLS=(
  https://github.com/ctrlpvim/ctrlp.vim.git
- https://github.com/kien/rainbow_parentheses.vim
- https://github.com/godlygeek/tabular
- https://github.com/mbbill/undotree
- https://github.com/vim-airline/vim-airline
- https://github.com/bronson/vim-trailing-whitespace
- https://github.com/valloric/youcompleteme
+ https://github.com/kien/rainbow_parentheses.vim.git
+ https://github.com/godlygeek/tabular.git
+ https://github.com/mbbill/undotree.git
+ https://github.com/vim-airline/vim-airline.git
+ https://github.com/bronson/vim-trailing-whitespace.git
+ https://github.com/valloric/youcompleteme.git
  https://github.com/tpope/vim-fugitive.git
  https://github.com/Valloric/vim-operator-highlight.git
  https://github.com/xolox/vim-easytags.git
@@ -28,42 +29,43 @@ cd ~/.config/nvim/bundle/
 for URL in ${GITURLS[@]}; do
     gitdir=$(basename "$URL")
     if [ ! -d $gitdir ] ; then
-        echo "$gitdir Already exists!"
+        echo "================================$gitdir Already exists! "
     else
-        echo "$gitdir not found, going to clone!"
+        echo "================================$gitdir not found, going to clone! "
 	#git clone "$URL"
     fi
 done
 
 #make sure we have nice font
 cd "$SOURCE/fonts"
-echo "Copying fonts to ~/.fonts/"
-if [! -d "~/.fonts"] ; then
+echo "================================Copying fonts to ~/.fonts/ "
+if [ ! -d "~/.fonts"] ; then
     mkdir ~/.fonts
 fi
 cp --verbose * ~/.fonts
 
 #/usr/share/nvim/runtime/colors
 #make sure we neovim stuffs
-echo "Linking nvim"
+echo "================================Linking nvim "
 cd "$SOURCE/nvim-plugs"
-ln --verbose dark.vim ~/.config/nvim/bundle/vim-airline/autoload/airline/themes/
-ln --verbose init.vim ~/.config/nvim/
-sudo ln --verbose snow.vim /usr/share/nvim/runtime/colors/snow.vim
+ln -fv dark.vim ~/.config/nvim/bundle/vim-airline/autoload/airline/themes/
+ln -fv init.vim ~/.config/nvim/
+echo "================================Need password to access /usr/share/nvim/runtime/colors/ to put in snow.vim"
+sudo ln -fv snow.vim /usr/share/nvim/runtime/colors/snow.vim
 
 #resources time
-echo "Linking bashrc and inputrc to home"
+echo "================================Linking bashrc and inputrc to home "
 cd "$SOURCE"
-ln --verbose .bashrc ~/.bashrc
-ln --verbose .inputrc ~/.inputrc
-ln --verbose .tmux.conf ~/.tmux.conf
+ln -fv .bashrc ~/.bashrc
+ln -fv .inputrc ~/.inputrc
+ln -fv .tmux.conf ~/.tmux.conf
 
-echo "Linking wallpapers to Pictures"
+echo "================================Linking wallpapers to Pictures "
 cd "$SOURCE/ims"
-ln --verbose elemental.jpg ~/Pictures
+ln -fv elemental.jpg ~/Pictures
 
-echo "And finally, terminix's json"
+echo "================================And finally, terminix's json "
 cd "$SOURCE/terminalix"
-ln --verbose snow.json ~/.config/terminix/
+ln -fv snow.json ~/.config/terminix/
 
-"Fin linking!"
+echo "================================Fin linking!
