06a86b85d0418f0ecac7ee1d757f6f06eca2adc9
joj <brandoncoffman.cs@gmail.com>
2017-02-22 13:02:27 -0500

    one day i will document but not today

--- install.sh
+++ install.sh
@@ -4,42 +4,41 @@ SOURCE=${PWD}
 PATHOGEN=~/.config/nvim/autoload
 if [ ! -d "$PATHOGEN" ] ; then
 	echo "================================curling pathogen! "
-	mkdir -p ~/.vim/autoload ~/.vim/bundle
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	mkdir -p ~/.config/nvim/autoload ~/.config/nvim/bundle
+	curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 else
 	echo "================================Already have pathogen; if not delete autoload, and rerun this. "
 fi
 
 #make sure we have all the plugins we need!
 declare -a GITURLS=(
- https://github.com/ctrlpvim/ctrlp.vim.git
- https://github.com/kien/rainbow_parentheses.vim.git
- https://github.com/godlygeek/tabular.git
- https://github.com/mbbill/undotree.git
- https://github.com/vim-airline/vim-airline.git
- https://github.com/bronson/vim-trailing-whitespace.git
- https://github.com/valloric/youcompleteme.git
- https://github.com/tpope/vim-fugitive.git
- https://github.com/Valloric/vim-operator-highlight.git
- https://github.com/xolox/vim-easytags.git
- https://github.com/xolox/vim-misc.git
+ https://github.com/ctrlpvim/ctrlp.vim
+ https://github.com/kien/rainbow_parentheses.vim
+ https://github.com/godlygeek/tabular
+ https://github.com/mbbill/undotree
+ https://github.com/vim-airline/vim-airline
+ https://github.com/bronson/vim-trailing-whitespace
+ https://github.com/valloric/youcompleteme
+ https://github.com/tpope/vim-fugitive
+ https://github.com/xolox/vim-easytags
+ https://github.com/xolox/vim-misc
  );
 
 cd ~/.config/nvim/bundle/
 for URL in ${GITURLS[@]}; do
     gitdir=$(basename "$URL")
-    if [ ! -d $gitdir ] ; then
+    if [ -d $gitdir ] ; then
         echo "================================$gitdir Already exists! "
     else
         echo "================================$gitdir not found, going to clone! "
-	#git clone "$URL"
+	git clone "$URL"
     fi
 done
 
 #make sure we have nice font
 cd "$SOURCE/fonts"
 echo "================================Copying fonts to ~/.fonts/ "
-if [ ! -d "~/.fonts"] ; then
+if [ ! -d ~/.fonts ] ; then
     mkdir ~/.fonts
 fi
 cp --verbose * ~/.fonts
@@ -68,4 +67,4 @@ echo "================================And finally, terminix's json "
 cd "$SOURCE/terminalix"
 ln -fv snow.json ~/.config/terminix/
 
-echo "================================Fin linking!
+echo "================================Fin linking!"
