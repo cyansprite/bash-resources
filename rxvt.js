7cb5b522882fee39022d901534572810bf4c3af8
Jojo Brandon Coffman <brandon.coffman@lexmark.com>
2018-01-17 15:46:42 -0500

    Fuck that stupid shit, cmd suffices

--- rxvt.js
+++
@@ -1,3 +0,0 @@
-var objShell = new ActiveXObject("shell.application");
-objShell.ShellExecute("C:\Program Files (x86)/Xming/Xming.exe", ":0 -clipboard -multiwindow", "", "open", 0);
-objShell.ShellExecute("bash.exe", "-c rxvt", "", "open", 0);
