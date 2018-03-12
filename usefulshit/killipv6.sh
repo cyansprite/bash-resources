f9254a94a5d87808777d8afaf05dfa3294297d70
Jojo Brandon Coffman <brandoncoffman.cs@gmail.com>
2018-03-12 07:38:09 -0400

    woot

---
+++ usefulshit/killipv6.sh
@@ -0,0 +1,7 @@
+echo "#disable ipv6" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+echo -n this should be 1 :
+cat /proc/sys/net/ipv6/conf/all/disable_ipv6
